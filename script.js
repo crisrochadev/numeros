3166154453a2671bbe95623c250bdf2395055eb9
Cris Rocha <70113780+crisrochadev@users.noreply.github.com>
2023-11-02 16:55:46 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const keyword = document.getElementById("keyword");
+const grid = document.getElementById("grid");
+let round = 0;
+let qtdKeys = 4;
+let currentInput = null;
+//Adiciona os numeros no grid
+function addNumber(num) {
+  if (currentInput) {
+    currentInput.textContent = num;
+  }
+}
+
+function mountGame() {
+  round++;
+  qtdKeys++;
+  grid.style.gridTemplateColumns = `repeat(${qtdKeys},1fr)`;
+  grid.style.gridTemplateRows = `repeat(${qtdKeys},1fr)`;
+  for (let num = 1; num <= qtdKeys * round; num++) {
+    const input = document.createElement("p");
+    input.name = "input_" + num;
+    input.id = "input_" + num;
+    input.classList.add("input");
+    input.type = "text";
+    input.inputMode = "number";
+    input.addEventListener("focus", (e) => {
+      currentInput = e.target;
+    });
+    input.tabIndex = num;
+
+    grid.appendChild(input);
+  }
+}
+
+window.onload = mountGame();
